3e223772fa527840a10dd4566d7327950795acd9
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-26 09:11:01 -0300

    checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,36 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'row',
+            gap: '$2',
+          }}
+        >
+          {Story()}
+          <Text size="sm">Label</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {},
+}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}

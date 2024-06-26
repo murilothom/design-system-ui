06b36255b9903eeea53991baf05cac2f10d7fa81
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-25 22:19:51 -0300

    react

---
+++ packages/react/src/components/Heading.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps } from '@stitches/react'
+import { styled } from '../styles'
+import { ElementType } from 'react'
+
+export const Heading = styled('h2', {
+  fontFamily: '$default',
+  lineHeight: '$shorter',
+  margin: 0,
+  color: '$gray100',
+
+  variants: {
+    size: {
+      sm: { fontSize: '$xl' },
+      md: { fontSize: '$2xl' },
+      lg: { fontSize: '$4xl' },
+      '2lg': { fontSize: '$5xl' },
+      '3lg': { fontSize: '$6xl' },
+      '4lg': { fontSize: '$7xl' },
+      '5lg': { fontSize: '$8xl' },
+      '6lg': { fontSize: '$9xl' },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export interface HeadingProps extends ComponentProps<typeof Heading> {
+  as?: ElementType
+}
